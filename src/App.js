e93dc46bb54b142087d13d982688cbf91e7e3eb0
jasonGhostDev <hyotiger777@gmail.com>
2021-10-26 00:24:18 +1000

    init website

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import Newdps from './newdps.png'
+function App() {
+  const [inputAddress, setInputAddress] = useState('');
+  const [msg, setMsg] = useState('');
+  let whitelist = require('./addresses_1.json');
+  const checkAddress = (addr) => {
+    for(var i=0;i<whitelist.length;i++){
+      if(whitelist[i] === addr)
+      { 
+        setMsg("Congratulations you are eligible for the mint!")
+        break;
+      }
+      setMsg("Sorry, you are not eligible for the mint.");
+    }
+  }
+  return (
+    
+    <div className="App">
+       <div id="drop-check-form">
+            <div id="img">
+                <img src={Newdps} alt=""></img>
+            </div> 
+            <form>
+            <input name="a" id="a" type="text" value={inputAddress} maxlength="42" placeholder="Address" onChange={(val) => setInputAddress(val.target.value)}/>
+            </form>
+           
+            <button id="send" onClick={()=>{checkAddress(inputAddress);}}>Check</button>
+            <span id="msg">{msg}</span>
+        </div>
+    </div>
+  );
+}
+
+export default App;
